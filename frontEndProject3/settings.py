eb896fbc7cf8f769b72c9884c0ce4849eac0fbed
olow304 <olow304@gmail.com>
2018-04-07 15:59:15 -0400

    Everything is connect now, showed welcome page.

--- frontEndProject3/settings.py
+++ frontEndProject3/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'twitter/static')
+    os.path.join(BASE_DIR, 'twitter/templates/')
 )
 
 MEDIA_URL = '/media/'
